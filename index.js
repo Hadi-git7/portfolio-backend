0bd6e1bb74445cbfbce0559b84b389224fce4b86
Hadi-git7 <abouhomeinh@gmail.com>
2023-01-21 11:26:53 +0200

    added to env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-import authorRouter from './routes/author.js';
+// import authorRouter from './routes/author.js';
 
 dotenv.config();
 
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.use('/api/author', authorRouter);
+// app.use('/api/author', authorRouter);
 
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`))
