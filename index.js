4a326541c1a215cc4d8cb01f721763b853e741b1
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2023-01-26 13:55:39 +0200

    fix conflict

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 import colors from 'colors'
 import projectRoutes from './routes/projectRoutes.js';
+import serviceRoutes from './routes/serviceRoutes.js'
+import announcementRoutes from './routes/announcementRoutes.js'
 
 
 dotenv.config();
@@ -22,7 +24,13 @@ if (process.env.NODE_ENV === "development"){
 
 app.use(express.json());
 
+app.use(express.urlencoded({extended : false}))
+
 app.use('/api/project', projectRoutes);
+
+app.use('/services',serviceRoutes)
+
+app.use('/announcements',announcementRoutes)
 
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`))
