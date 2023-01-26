29073f91d4082aacf364b38f60c8302ea0ea7b6a
Hadi-git7 <abouhomeinh@gmail.com>
2023-01-26 14:47:35 +0200

    Added route to announcement

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -1,8 +1,9 @@
 import express  from 'express';
 const router = express.Router();
-import {getAnnouncement,setAnnouncement,updateAnnouncement,deleteAnnouncement,upload} from '../controllers/announcementController.js';
+import {getAnnouncement,setAnnouncement,updateAnnouncement,deleteAnnouncement,upload, getAnnouncementById} from '../controllers/announcementController.js';
 
 router.route('/').get(getAnnouncement)
+router.route('/:id').get(getAnnouncementById)
 router.route('/').post(upload.single("image"),setAnnouncement)
 router.route('/:id').put(upload.single("image"),updateAnnouncement)
 router.route('/:id').delete(deleteAnnouncement)
