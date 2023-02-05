1a242ce1d8cb514d768a2e96a3fd7659dc5ca1e7
Hadi-git7 <abouhomeinh@gmail.com>
2023-02-05 14:15:06 +0200

    push before frontend push

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 import router from './routes/contactRoutes.js';
 import colors from 'colors'
+import cors from 'cors'
 
 import serviceRoutes from './routes/serviceRoutes.js'
 import announcementRoutes from './routes/announcementRoutes.js'
@@ -24,7 +25,9 @@ if (process.env.NODE_ENV === "development") {
 //middle ware 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}))
-
+app.use(cors())
+app.use(express.static('uploads'));
+app.use('/uploads', express.static('uploads'))
 
 app.use('/api/contact', router);
 
